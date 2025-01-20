3d438577a031cbf658236fd0511f249a3c1a1ed2
Valentin Lucet <valentin.lucet@gmail.com>
2025-01-20 14:28:05 +0000

    sweeps progress

--- scripts/bash/run.sh
+++ scripts/bash/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-export PYTHONPATH=.
+# export PYTHONPATH=.
 
-python mdclassifier/train.py --config configs/config.yaml
+# python mdclassifier/train.py --config configs/config.yaml
+
+wandb sweep --name bigsweep --project cv4e-sweep configs/sweep.yaml
