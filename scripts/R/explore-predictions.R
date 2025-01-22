74b5b4dc8a3a1f216ed001f34108c1e6149fd56e
Valentin Lucet <valentin.lucet@gmail.com>
2025-01-22 09:28:33 -0800

    add r code

---
+++ scripts/R/explore-predictions.R
@@ -0,0 +1,34 @@
+library(readr)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+
+cats = c("NONE",
+         "STAF",
+         "CARI",
+         "GOOS",
+         "DUCK",
+         "MOOS",
+         "SAND",
+         "BIRD",
+         "GROU",
+         "MAMM",
+         "BEAR",
+         "MUST")
+
+color_table
+
+preds = read_csv("outputs/predictions.csv") %>%
+  select(-`...1`) %>%
+  rename(truth = label_group) %>%
+  pivot_longer(cols = NONE:MUST, values_to = "score", names_to = "pred_category") %>%
+  mutate(pred_category = factor(pred_category, levels = cats))
+
+head(preds)
+
+ggplot(preds) +
+  geom_density(aes(x = score, fill=pred_category),
+               alpha = 0.2) +
+  facet_wrap(~truth) +
+  theme_minimal() +
+  scale_fill_manual(values = c(c("#E69F00", rep("#56B4E9", 11))))
