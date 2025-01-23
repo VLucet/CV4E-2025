739491a57a61ae95819baffcb3e61b4ed50593e3
Valentin Lucet <valentin.lucet@gmail.com>
2025-01-22 16:45:47 -0800

    updates

--- scripts/R/explore-predictions.R
+++ scripts/R/explore-predictions.R
@@ -2,33 +2,48 @@ library(readr)
 library(ggplot2)
 library(dplyr)
 library(tidyr)
+library(sigmoid)
 
-cats = c("NONE",
-         "STAF",
-         "CARI",
-         "GOOS",
-         "DUCK",
-         "MOOS",
-         "SAND",
-         "BIRD",
-         "GROU",
-         "MAMM",
-         "BEAR",
-         "MUST")
-
-color_table
+lookup = read_csv("data/tabular/labels_lookup.csv")
 
 preds = read_csv("outputs/predictions.csv") %>%
-  select(-`...1`) %>%
-  rename(truth = label_group) %>%
+  rename(truth = label_group, sample = `...1`) %>%
   pivot_longer(cols = NONE:MUST, values_to = "score", names_to = "pred_category") %>%
-  mutate(pred_category = factor(pred_category, levels = cats))
+  group_by(sample) %>%
+  mutate(max_score = max(score)) %>%
+  mutate(pred = pred_category[score == max_score]) %>%
+  ungroup() %>%
+  mutate(correct = truth == pred) %>%
+  mutate(pred_category = factor(pred_category, levels = lookup$label_group)) %>%
+  # pivot_wider(names_from = pred_category, values_from = score) %>%
+  select(-max_score)
 
 head(preds)
 
-ggplot(preds) +
-  geom_density(aes(x = score, fill=pred_category),
-               alpha = 0.2) +
-  facet_wrap(~truth) +
-  theme_minimal() +
-  scale_fill_manual(values = c(c("#E69F00", rep("#56B4E9", 11))))
+alpha = 0.5
+
+for (cls in lookup$label_group) {
+  x = ggplot() +
+    geom_histogram(
+      data = preds %>%
+        filter(truth == cls,
+               pred_category == cls),
+      aes(x = score, fill = "POS"), alpha = alpha)+
+    geom_histogram(
+      data = preds %>%
+        filter(truth != cls,
+               pred_category == cls),
+      aes(x = score, fill = "NEG"), alpha = alpha)+
+    theme_minimal() +
+    ggtitle(cls)
+  plot(x)
+}
+
+ggplot() +
+  geom_histogram(
+    data = preds %>% filter(correct),
+    aes(x = logistic(SAND), fill = correct), alpha = alpha)+
+  geom_histogram(
+    data = preds %>% filter(!correct),
+    aes(x = logistic(SAND), fill = correct), alpha = alpha)+
+  theme_minimal()
